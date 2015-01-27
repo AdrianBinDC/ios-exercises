1c2be829144988f18b5a69c25435de9b4456abb2
Adrian Bolinger <adrian.bolinger@me.com>
2015-01-27 15:17:08 -0500

    Completed if, else, ternary

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,34 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    // If number and otherNumber are equal, return number (or otherNumber)
+    // THIS WORKS!
+    if (number == otherNumber) {
+        return [NSString stringWithFormat:@"%ld", number];
+    }
+    
+    // Set up variables for high/low numbers
+    NSInteger lowNumber;
+    NSInteger highNumber;
+    
+    // Test to see which number is lower/higher & assign variable
+    if (number < otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        highNumber = number;
+        lowNumber = otherNumber;
+    }
+    
+    NSMutableString *newString = [NSMutableString string];
+    
+    // Construct the string starting at lowNumber, going to highNumber
+    // sets starting point, end point, and interval
+    for (NSInteger i = lowNumber; i <= highNumber; i++) {
+        [newString appendFormat:@"%ld", (long)i];
+    }
+    
+    return newString;
 }
 
 @end
