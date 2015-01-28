da6a6dd894402d03fbf6bca6b8a98420811e4649
Adrian Bolinger <adrian.bolinger@me.com>
2015-01-28 07:41:54 -0500

    Completed dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,29 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    return [characterDictionary valueForKey:@"favorite drink"];
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    // create an array to dump favorite drinks
+    NSMutableArray *favoriteDrinkArray = [NSMutableArray array];
+    
+    // cycle through each characterDictionary in charactersArray & grab favorite drink
+    for (NSDictionary *characterDictionary in charactersArray) {
+        [favoriteDrinkArray addObject:[characterDictionary valueForKey:@"favorite drink"]];
+    }
+    return favoriteDrinkArray;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    // create a mutable dictionary from character dictionary
+    NSMutableDictionary *mutableCharacterDictionary = [characterDictionary mutableCopy];
+    // add a quote
+    mutableCharacterDictionary[@"quote"] = @"The quick brown fox jumped over the lazy dog";
+    // return the quote
+    return mutableCharacterDictionary;
 }
 
 @end
