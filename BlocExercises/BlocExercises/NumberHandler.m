3be70015ab8795e2a2deea06f9a239f1a055f227
Adrian Bolinger <adrian.bolinger@me.com>
2015-01-28 09:50:38 -0500

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,49 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    return @([number floatValue] * 2);
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
+    
+    // Create variables for high & low numbers
+    // This makes code more resilient if otherNumber is lower than number
+    NSInteger lowNumber, highNumber;
+    
+    if (number < otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        lowNumber = otherNumber;
+        highNumber = number;
+    }
+    
+    NSMutableArray *numberArray = [NSMutableArray array];
+    for (long i = lowNumber; i <= highNumber ; i++) {
+        [numberArray addObject:@(i)];
+    }
+    
     /* WORK HERE */
-    return @[];
+    return numberArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    // create a variable for hte lowest number
+    NSInteger smallestNumber = NSIntegerMax;
+    
+    // run through the array of numbers comparing integer value to current smallestNumber
+    for (NSNumber *number in arrayOfNumbers) {
+        NSInteger integerValue = [number integerValue];
+        
+        // if current integer is smaller than smallestNumber, it's assigned as smallestNumber
+        if (integerValue < smallestNumber) {
+            smallestNumber = integerValue;
+        }
+    }
+    
+    // returns the smallelstNumber after method's gone thru array    
+    return smallestNumber;
 }
 
 @end
