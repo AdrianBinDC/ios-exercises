7a33b0056ba07ca536218e3bcf6137611278e94e
Adrian Bolinger <adrian.bolinger@me.com>
2015-01-27 20:27:34 -0500

    Completed arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,21 +12,30 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    return [characterString componentsSeparatedByString:@";"];
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    return [characterArray componentsJoinedByString:@";"];
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    NSSortDescriptor *alphabetize = [[NSSortDescriptor alloc]initWithKey:nil ascending:YES];
+    return [characterArray sortedArrayUsingDescriptors:@[alphabetize]];
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+    for (NSString *characterName in characterArray) {
+        NSString *lowerCaseCharacter = characterName.lowercaseString;
+        NSRange worfRange = [lowerCaseCharacter rangeOfString:@"worf"];
+        
+        if (worfRange.location != NSNotFound) {
+            return YES;
+        }
+    }
     return NO;
 }
 
